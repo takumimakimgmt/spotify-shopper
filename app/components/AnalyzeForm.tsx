6c34de26ed57381ac6dfcb6bbbfcda5f720c98d3
Takumi Maki <takumimaki.mgmt@gmail.com>
2025-12-31 10:56:51 +0900

    fix: make AnalyzeForm onlyUnowned props optional

--- app/components/AnalyzeForm.tsx
+++ app/components/AnalyzeForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import ProcessingBar from './ProcessingBar';
 import ErrorAlert from './ErrorAlert';
 import { ProgressItem } from './ProgressList';
 import { MAX_XML_BYTES } from '@/lib/constants';
@@ -12,7 +11,7 @@ export interface AnalyzeFormProps {
   // State
   playlistUrlInput: string;
   rekordboxFile: File | null;
-  onlyUnowned: boolean;
+  onlyUnowned?: boolean;
   rekordboxDate?: string | null;
   rekordboxFilename?: string | null;
   loading: boolean;
@@ -26,7 +25,7 @@ export interface AnalyzeFormProps {
   // Setters
   setPlaylistUrlInput: (value: string) => void;
   setRekordboxFile: (file: File | null) => void;
-  setOnlyUnowned: (value: boolean) => void;
+  setOnlyUnowned?: (value: boolean) => void;
   // Handlers
   handleAnalyze: (e: React.FormEvent) => void;
   handleRekordboxChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -254,13 +253,15 @@ export default function AnalyzeForm(props: AnalyzeFormProps) {
               <label className="relative inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
-                  checked={props.onlyUnowned}
-                  onChange={e => props.setOnlyUnowned(e.target.checked)}
+                  checked={!!props.onlyUnowned}
+                  onChange={(e) => props.setOnlyUnowned?.(e.target.checked)}
                   className="sr-only peer"
+{props.setOnlyUnowned && (
                   id="only-unowned"
                 />
                 <div className="w-11 h-6 bg-white/10 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500/40 rounded-full peer peer-checked:bg-blue-500 transition-all"></div>
-                <div className="absolute left-0.5 top-0.5 bg-white w-5 h-5 rounded-full shadow-md transition-all peer-checked:translate-x-5"></div>
+
+)}                <div className="absolute left-0.5 top-0.5 bg-white w-5 h-5 rounded-full shadow-md transition-all peer-checked:translate-x-5"></div>
               </label>
             </div>
           </div>
